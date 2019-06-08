5258c65a6daa5c5eca21acf7ad0de3c06220e99d
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-08 03:36:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(some_array)
   some_array = [1,2,3,4,5]
-  some_array.each math.sqrt |(some_array)|
-    new_array =
+  some_array.each 
+    new_array = 
 end
 
 arr = [1, 2, 3, 4, 5]
